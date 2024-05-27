4965b6fdb7c50130f57f22c4ff744185f227202c
Simran Shrestha <simranstha2u@gmail.com>
2024-05-27 08:33:36 +0545

    navbar menu responsive done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiLocationOn, CiSearch } from "react-icons/ci";
-import { FaAngleDown } from "react-icons/fa6";
-// import { Link } from "react-router-dom";
+import { FaAngleDown, FaBars, FaTimes } from "react-icons/fa";
 import { GoHeart } from "react-icons/go";
-
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { LuPhoneCall } from "react-icons/lu";
 import { NavLink } from "react-router-dom";
 import plant from "../../assets/image/plant.jpg";
+
 function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   const navItems = [
     { link: "Home", path: "/" },
     { link: "Shop", path: "/shop" },
@@ -20,7 +21,7 @@ function Navbar() {
 
   return (
     <section className="">
-      <div className=" w-full">
+      <div className="w-full">
         <div className="border-b border-grey">
           <div className="flex justify-between mt-5 mb-2 md:mx-auto md:px-28 px-3">
             <div className="flex items-center gap-2 text-[#4D4D4D]">
@@ -28,7 +29,7 @@ function Navbar() {
               Store Location: Lincoln-344, Illinois, Chicago, USA
             </div>
             <div className="flex gap-7">
-              <div className="flex  gap-2 border-r-2 border-grey ">
+              <div className="flex gap-2 border-r-2 border-grey ">
                 <div className="flex items-center mr-3 gap-2 text-[#4D4D4D]">
                   ENG <FaAngleDown />
                   USD <FaAngleDown />
@@ -60,9 +61,9 @@ function Navbar() {
         </div>
         <div className="flex items-center justify-between text-center mt-5 mb-2 md:mx-auto md:px-28 px-3">
           {/* logo */}
-          <div className=" text-center">
+          <div className="text-center">
             <a className="h-8 w-8 text-2xl font-semibold flex items-center space-x-3">
-              <img src={plant} />
+              <img src={plant} alt="Ecobazar Logo" />
               <NavLink to="/">
                 <span>Ecobazar</span>
               </NavLink>
@@ -92,25 +93,17 @@ function Navbar() {
           </div>
           {/* like */}
           <div className="flex items-center justify-center">
-            <div className="   ">
-              <div className="flex pr-2 border-r-2 border-grey">
-                <div className="">
-                  <GoHeart size={30} />
-                </div>
-              </div>
+            <div className="flex pr-2 border-r-2 border-grey">
+              <GoHeart size={30} />
             </div>
-           
             <div className="relative w-7 h-7 ml-2">
-            <NavLink to="/cart">
-              <HiOutlineShoppingBag size={30} />
+              <NavLink to="/cart">
+                <HiOutlineShoppingBag size={30} />
               </NavLink>
               <div className="absolute top-[1px] right-[-2px] bg-primary w-4 h-4 rounded-full flex justify-center items-center text-[10px] text-white">
                 2
               </div>
-              
             </div>
-            
-
             <div className="text-left ml-2">
               <p className="text-[12px] leading-[13.2px] font-normal text-[#4D4D4D]">
                 Shopping Cart
@@ -118,29 +111,55 @@ function Navbar() {
               <p className="text-[14px] leading-[14px] font-medium">$57.0</p>
             </div>
           </div>
+          <div className="mobile-menu-button">
+            <button
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+              className="focus:outline-none"
+            >
+              {isMobileMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+            </button>
+          </div>
         </div>
 
-        <div className="flex justify-between text-center mt-5 md:mx-auto md:px-28  bg-[#333333]  ">
-          <div>
-            <ul className="text-grey md:flex space-x-8 m-5">
-              {navItems.map((value, index) => (
-                <li key={index}>
-                  <NavLink
-                    to={value.path}
-                    className={({ isActive }) =>
-                      isActive ? "text-green-500" : "text-white"
-                    }
-                  >
-                    {value.link}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </div>
+        <div
+          className={`mobile-menu ${
+            isMobileMenuOpen ? "mobile-menu-open" : ""
+          } bg-[#333333] text-white`}
+        >
+          <ul className="flex flex-col items-center space-y-5 py-5">
+            {navItems.map((value, index) => (
+              <li key={index}>
+                <NavLink
+                  to={value.path}
+                  className={({ isActive }) =>
+                    isActive ? "text-green-500" : "text-white"
+                  }
+                  onClick={() => setIsMobileMenuOpen(false)}
+                >
+                  {value.link}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="desktop-menu flex justify-between text-center mt-5 md:mx-auto md:px-28 bg-[#333333]">
+          <ul className="text-grey md:flex space-x-8 m-5">
+            {navItems.map((value, index) => (
+              <li key={index}>
+                <NavLink
+                  to={value.path}
+                  className={({ isActive }) =>
+                    isActive ? "text-green-500" : "text-white"
+                  }
+                >
+                  {value.link}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
           <div className="m-5 md:flex text-grey gap-2">
-            <div>
-              <LuPhoneCall size={20} />
-            </div>
+            <LuPhoneCall size={20} />
             <div>(219) 555-0114</div>
           </div>
         </div>
