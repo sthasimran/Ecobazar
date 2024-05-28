ff2fc10b3a95ad4a80059ed1a3ee82973481b933
Simran Shrestha <simranstha2u@gmail.com>
2024-05-28 09:04:09 +0545

    cart page responsive done

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -10,105 +10,131 @@ const Cart = () => {
   return (
     <div>
       <Breadcrumbs />
-      <section className=" m-12 md:mx-auto md:px-28 px-3">
+      <section className="m-4 md:mx-auto md:px-28 px-3">
         <div>
           <p className="text-center text-[32px] leading-9 font-semibold">
             My Shopping Cart
           </p>
         </div>
 
-        <div className="grid grid-cols-3 gap-6 mt-5">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-5">
 
-          <div className=" col-span-2">
+          <div className="col-span-2">
             <div>
-            <div className="overflow-auto rounded-lg shadow md:block">
-              <table className="w-full">
-                <thead className=" border-b-2 border-gray-200">
-                  <tr>
-                    <th className="p-3 text-sm font-medium text-[#808080] tracking-wide text-left">
-                      PRODUCT
-                    </th>
-                    <th className="w-24 p-3 text-sm font-medium text-[#808080] tracking-wide text-left">
-                      PRICE
-                    </th>
-                    <th className="w-24 p-3 text-sm font-medium text-[#808080] tracking-wide text-left">
-                      QUANTITY
-                    </th>
-                    <th className="w-32 p-3 text-sm font-medium text-[#808080] tracking-wide text-left">
-                      SUBTOTAL
-                    </th>
-                  </tr>
-                </thead>
-                <tbody className="divide-y border-b">
-                  <tr className="bg-white">
-                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-                      <div className="flex items-center gap-2">
-                        <img
-                          src={greenCapsicum}
-                          alt="green"
-                          className="w-[100px] h-[100px]"
-                        />
-                        <p className="text-[#1A1A1A] text-base font-normal">
-                          Green Capsicum
-                        </p>
-                      </div>
-                    </td>
-                    <td className="p-3 text-[#1A1A1A] text-base font-normal whitespace-nowrap">
-                      $14.00
-                    </td>
-                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+              <div className="overflow-auto rounded-lg shadow">
+                <table className="w-full hidden md:table">
+                  <thead className="border-b-2 border-gray-200">
+                    <tr>
+                      <th className="p-3 text-sm font-medium text-[#808080] tracking-wide text-left">
+                        PRODUCT
+                      </th>
+                      <th className="w-24 p-3 text-sm font-medium text-[#808080] tracking-wide text-left">
+                        PRICE
+                      </th>
+                      <th className="w-24 p-3 text-sm font-medium text-[#808080] tracking-wide text-left">
+                        QUANTITY
+                      </th>
+                      <th className="w-32 p-3 text-sm font-medium text-[#808080] tracking-wide text-left">
+                        SUBTOTAL
+                      </th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y border-b">
+                    <tr className="bg-white">
+                      <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                        <div className="flex items-center gap-2">
+                          <img
+                            src={greenCapsicum}
+                            alt="green"
+                            className="w-[100px] h-[100px]"
+                          />
+                          <p className="text-[#1A1A1A] text-base font-normal">
+                            Green Capsicum
+                          </p>
+                        </div>
+                      </td>
+                      <td className="p-3 text-[#1A1A1A] text-base font-normal whitespace-nowrap">
+                        $14.00
+                      </td>
+                      <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                        <span className="p-1.5 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 rounded-lg bg-opacity-50">
+                          Counter
+                        </span>
+                      </td>
+                      <td className="p-3 text-[#1A1A1A] text-base font-normal whitespace-nowrap">
+                        $70.00
+                      </td>
+                      <td className="p-3">
+                        <TiDeleteOutline size={25} color="#666666" />
+                      </td>
+                    </tr>
+                    <tr className="bg-white">
+                      <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                        <div className="flex items-center gap-2">
+                          <img
+                            src={redCapsicum}
+                            alt="green"
+                            className="w-[100px] h-[100px]"
+                          />
+                          <p className="text-[#1A1A1A] text-base font-normal">
+                            Green Capsicum
+                          </p>
+                        </div>
+                      </td>
+                      <td className="p-3 text-[#1A1A1A] text-base font-normal whitespace-nowrap">
+                        $14.00
+                      </td>
+                      <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                        <span className="p-1.5 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 rounded-lg bg-opacity-50">
+                          Counter
+                        </span>
+                      </td>
+                      <td className="p-3 text-[#1A1A1A] text-base font-normal whitespace-nowrap">
+                        $14.00
+                      </td>
+                      <td className="p-3">
+                        <TiDeleteOutline size={25} color="#666666" />
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div className="md:hidden">
+                  <div className="flex items-center justify-between mb-4">
+                    <img src={greenCapsicum} alt="green" className="w-24 h-24"/>
+                    <div>
+                      <p className="text-[#1A1A1A] text-base font-normal">Green Capsicum</p>
+                      <p className="text-[#1A1A1A] text-base font-normal">$14.00</p>
                       <span className="p-1.5 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 rounded-lg bg-opacity-50">
                         Counter
                       </span>
-                    </td>
-                    <td className="p-3 text-[#1A1A1A] text-base font-normal whitespace-nowrap">
-                      $70.00
-                    </td>
-                    <td className="">
-                      <TiDeleteOutline size={25} color="#666666" />
-                    </td>
-                  </tr>
-                  <tr className="bg-white">
-                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-                      <div className="flex items-center gap-2">
-                        <img
-                          src={redCapsicum}
-                          alt="green"
-                          className="w-[100px] h-[100px]"
-                        />
-
-                        <p className="text-[#1A1A1A] text-base font-normal">
-                          Green Capsicum
-                        </p>
-                      </div>
-                    </td>
-                    <td className="p-3 text-[#1A1A1A] text-base font-normal whitespace-nowrap">
-                      $14.00
-                    </td>
-                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
+                      <p className="text-[#1A1A1A] text-base font-normal">$70.00</p>
+                    </div>
+                    <TiDeleteOutline size={25} color="#666666" />
+                  </div>
+                  <div className="flex items-center justify-between mb-4">
+                    <img src={redCapsicum} alt="red" className="w-24 h-24"/>
+                    <div>
+                      <p className="text-[#1A1A1A] text-base font-normal">Red Capsicum</p>
+                      <p className="text-[#1A1A1A] text-base font-normal">$14.00</p>
                       <span className="p-1.5 text-xs font-medium uppercase tracking-wider text-green-800 bg-green-200 rounded-lg bg-opacity-50">
                         Counter
                       </span>
-                    </td>
-                    <td className="p-3 text-[#1A1A1A] text-base font-normal whitespace-nowrap">
-                      $14.00
-                    </td>
-                    <td className="">
-                      <TiDeleteOutline size={25} color="#666666" />
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div className="flex item-center justify-between my-3 p-2 border">
-                <div className="bg-[#F2F2F2] p-3 rounded-full text-sm font-semibold text-[#4D4D4D]">
+                      <p className="text-[#1A1A1A] text-base font-normal">$14.00</p>
+                    </div>
+                    <TiDeleteOutline size={25} color="#666666" />
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row items-center justify-between my-3 p-2 border">
+                <div className="bg-[#F2F2F2] p-3 rounded-full text-sm font-semibold text-[#4D4D4D] cursor-pointer mb-2 md:mb-0">
                   Return to shop
                 </div>
-                <div className="bg-[#F2F2F2] p-3 rounded-full text-sm font-semibold text-[#4D4D4D]">
+                <div className="bg-[#F2F2F2] p-3 rounded-full text-sm font-semibold text-[#4D4D4D] cursor-pointer">
                   Update Cart
                 </div>
               </div>
-              </div>
+            </div>
             <Coupon />
           </div>
 
@@ -123,27 +149,22 @@ const Cart = () => {
                         <th className="px-4 py-2 border-b text-left text-[#4D4D4D] text-sm font-normal">
                           Subtotal:
                         </th>
-
                         <td className="px-4 py-2 border-b text-[#1A1A1A] text-sm font-semibold">
                           $84.00
                         </td>
                       </tr>
-
                       <tr>
                         <th className="px-4 py-2 border-b text-left text-[#4D4D4D] text-sm font-normal">
                           Shipping:
                         </th>
-
                         <td className="px-4 py-2 border-b text-[#1A1A1A] text-sm font-semibold">
                           Free
                         </td>
                       </tr>
-
                       <tr>
                         <th className="px-4 py-2 border-b text-left text-[#4D4D4D] text-sm font-normal">
                           Total:
                         </th>
-
                         <td className="px-4 py-2 border-b text-[#1A1A1A] text-sm font-semibold">
                           $84.00
                         </td>
@@ -154,7 +175,7 @@ const Cart = () => {
                 <div className="my-4">
                   <Button
                     type="submit"
-                    className="w-full bg-primary rounded-full  hover:bg-[#2C742F] duration-150 "
+                    className="w-full bg-primary rounded-full hover:bg-[#2C742F] duration-150"
                   >
                     Proceed to checkout
                   </Button>
--- src/Pages/Cart/Coupon.jsx
+++ src/Pages/Cart/Coupon.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const Coupon = () => {
   return (
-    <div className="flex items-center justify-between border rounded-lg p-2 mt-4">
-      <p className="text-[#1A1A1A] text-[20px] font-medium">Coupon Code</p>
-      <div>
+    <div className="flex flex-col md:flex-row items-center justify-between border rounded-lg p-2 mt-4">
+      <p className="text-[#1A1A1A] text-[20px] font-medium mb-2 md:mb-0">
+        Coupon Code
+      </p>
+      <div className="flex flex-col md:flex-row items-center w-full md:w-auto">
         <input
           type="text"
           placeholder="Enter code"
-          className="border rounded-3xl px-[32px] py-[10px] w-[400px]"
+          className="border rounded-3xl px-4 py-2 w-full md:w-[300px] mb-2 md:mb-0 md:mr-2"
         />
-        <button className="bg-[#333333] border rounded-full px-[32px] py-[10px] text-[#FFFFFF] text-base font-semibold ml-[-40px]">
+        <button className="bg-[#333333] border rounded-full px-6 py-2 text-[#FFFFFF] text-base font-semibold">
           Apply Coupon
         </button>
       </div>
